48eb3fe6d13ba1032f5d996549a5013dfcf308d6
kyle merfeld <kmerfeld1@gmail.com>
2017-04-21 17:43:21 -0500

    hopefully fixes indentation and other linting stuff

--- openstack-gerrit/tf.py
+++ openstack-gerrit/tf.py
@@ -33,146 +33,144 @@ CONTINUOUS_COLUMNS = ["deletions", "insertions"]
 
 
 def build_estimator(model_dir, model_type):
-  """Build an estimator."""
-  # Sparse base columns.
-  verified = tf.contrib.layers.sparse_column_with_keys(column_name="verified",
-                                                     keys=["-1", "1"])
-  project = tf.contrib.layers.sparse_column_with_hash_bucket(
+    """Build an estimator."""
+    # Sparse base columns.
+    verified = tf.contrib.layers.sparse_column_with_keys(column_name="verified", keys=["-1", "1"])
+    project = tf.contrib.layers.sparse_column_with_hash_bucket(
       "project", hash_bucket_size=100)
-  accountid = tf.contrib.layers.sparse_column_with_hash_bucket(
+    accountid = tf.contrib.layers.sparse_column_with_hash_bucket(
       "accountid", hash_bucket_size=100)
-  changeid = tf.contrib.layers.sparse_column_with_hash_bucket(
+    changeid = tf.contrib.layers.sparse_column_with_hash_bucket(
       "changeid", hash_bucket_size=1000)
 
-  # Continuous base columns.
-  deletions = tf.contrib.layers.real_valued_column("deletions")
-  insertions = tf.contrib.layers.real_valued_column("insertions")
+    # Continuous base columns.
+    deletions = tf.contrib.layers.real_valued_column("deletions")
+    insertions = tf.contrib.layers.real_valued_column("insertions")
 
-  # Wide columns and deep columns.
-  wide_columns = [accountid, changeid, project, verified]
-  deep_columns = [
+    # Wide columns and deep columns.
+    wide_columns = [accountid, changeid, project, verified]
+    deep_columns = [
       tf.contrib.layers.embedding_column(accountid, dimension=8),
       tf.contrib.layers.embedding_column(changeid, dimension=8),
       tf.contrib.layers.embedding_column(verified, dimension=8),
       tf.contrib.layers.embedding_column(project, dimension=8),
       deletions,
       insertions,
-  ]
+    ]
 
-  if model_type == "wide":
-    m = tf.contrib.learn.LinearClassifier(model_dir=model_dir,
+    if model_type == "wide":
+        m = tf.contrib.learn.LinearClassifier(model_dir=model_dir,
                                           feature_columns=wide_columns)
-  elif model_type == "deep":
-    m = tf.contrib.learn.DNNClassifier(model_dir=model_dir,
+    elif model_type == "deep":
+        m = tf.contrib.learn.DNNClassifier(model_dir=model_dir,
                                        feature_columns=deep_columns,
                                        hidden_units=[100, 50])
-  else:
-    m = tf.contrib.learn.DNNLinearCombinedClassifier(
+    else:
+        m = tf.contrib.learn.DNNLinearCombinedClassifier(
         model_dir=model_dir,
         linear_feature_columns=wide_columns,
         dnn_feature_columns=deep_columns,
-        dnn_hidden_units=[100, 50])
         dnn_hidden_units=[100, 50],
         fix_global_step_increment_bug=True)
-  return m
+    return m
 
 
 def input_fn(df):
- """Input builder function."""
-  # Creates a dictionary mapping from each continuous feature column name (k) to
-  # the values of that column stored in a constant Tensor.
-  continuous_cols = {k: tf.constant(df[k].values) for k in CONTINUOUS_COLUMNS}
-  # Creates a dictionary mapping from each categorical feature column name (k)
-  # to the values of that column stored in a tf.SparseTensor.
-  categorical_cols = {
+    """Input builder function."""
+    # Creates a dictionary mapping from each continuous feature column name (k) to
+    # the values of that column stored in a constant Tensor.
+    continuous_cols = {k: tf.constant(df[k].values) for k in CONTINUOUS_COLUMNS}
+    # Creates a dictionary mapping from each categorical feature column name (k)
+    # to the values of that column stored in a tf.SparseTensor.
+    categorical_cols = {
       k: tf.SparseTensor(
           indices=[[i, 0] for i in range(df[k].size)],
           values=df[k].values,
           dense_shape=[df[k].size, 1])
       for k in CATEGORICAL_COLUMNS}
-  # Merges the two dictionaries into one.
-  feature_cols = dict(continuous_cols)
-  feature_cols.update(categorical_cols)
-  # Converts the label column into a constant Tensor.
-  label = tf.constant(df[LABEL_COLUMN].values)
-  # Returns the feature columns and the label.
-  return feature_cols, label
+    # Merges the two dictionaries into one.
+    feature_cols = dict(continuous_cols)
+    feature_cols.update(categorical_cols)
+    # Converts the label column into a constant Tensor.
+    label = tf.constant(df[LABEL_COLUMN].values)
+    # Returns the feature columns and the label.
+    return feature_cols, label
 
 
 def train_and_eval(model_dir, model_type, train_steps, train_data, test_data):
-  """Train and evaluate the model."""
-  # train_file_name, test_file_name = "changes.csv"
-  df_train = pd.read_csv(
-      tf.gfile.Open("changes.csv"),
-      names=COLUMNS,
-      skipinitialspace=True,
-      engine="python")
-  df_test = pd.read_csv(
-      tf.gfile.Open("changes.csv"),
-      names=COLUMNS,
-      skipinitialspace=True,
-      skiprows=1,
-      engine="python")
+    """Train and evaluate the model."""
+    # train_file_name, test_file_name = "changes.csv"
+    df_train = pd.read_csv(
+    tf.gfile.Open("changes.csv"),
+    names=COLUMNS,
+    skipinitialspace=True,
+    engine="python")
+    df_test = pd.read_csv(
+    tf.gfile.Open("changes.csv"),
+    names=COLUMNS,
+    skipinitialspace=True,
+    skiprows=1,
+    engine="python")
 
-  # remove NaN elements
-  df_train = df_train.dropna(how='any', axis=0)
-  df_test = df_test.dropna(how='any', axis=0)
+    # remove NaN elements
+    df_train = df_train.dropna(how='any', axis=0)
+    df_test = df_test.dropna(how='any', axis=0)
 
-  df_train[LABEL_COLUMN] = (
-      df_train["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
-  df_test[LABEL_COLUMN] = (
-      df_test["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
+    df_train[LABEL_COLUMN] = (
+    df_train["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
+    df_test[LABEL_COLUMN] = (
+    df_test["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
 
-  model_dir = tempfile.mkdtemp() if not model_dir else model_dir
-  print("model directory = %s" % model_dir)
+    model_dir = tempfile.mkdtemp() if not model_dir else model_dir
+    print("model directory = %s" % model_dir)
 
-  m = build_estimator(model_dir, model_type)
-  m.fit(input_fn=lambda: input_fn(df_train), steps=train_steps)
-  results = m.evaluate(input_fn=lambda: input_fn(df_test), steps=1)
-  for key in sorted(results):
-    print("%s: %s" % (key, results[key]))
+    m = build_estimator(model_dir, model_type)
+    m.fit(input_fn=lambda: input_fn(df_train), steps=train_steps)
+    results = m.evaluate(input_fn=lambda: input_fn(df_test), steps=1)
+    for key in sorted(results):
+        print("%s: %s" % (key, results[key]))
 
 
 FLAGS = None
 
 
 def main(_):
-  train_and_eval(FLAGS.model_dir, FLAGS.model_type, FLAGS.train_steps,
+    train_and_eval(FLAGS.model_dir, FLAGS.model_type, FLAGS.train_steps,
                  FLAGS.train_data, FLAGS.test_data)
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.register("type", "bool", lambda v: v.lower() == "true")
-  parser.add_argument(
+    parser = argparse.ArgumentParser()
+    parser.register("type", "bool", lambda v: v.lower() == "true")
+    parser.add_argument(
       "--model_dir",
       type=str,
       default="",
       help="Base directory for output models."
-  )
-  parser.add_argument(
+    )
+    parser.add_argument(
       "--model_type",
       type=str,
       default="wide_n_deep",
       help="Valid model types: {'wide', 'deep', 'wide_n_deep'}."
-  )
-  parser.add_argument(
+    )
+    parser.add_argument(
       "--train_steps",
       type=int,
       default=200,
       help="Number of training steps."
-  )
-  parser.add_argument(
+    )
+    parser.add_argument(
       "--train_data",
       type=str,
       default="",
       help="Path to the training data."
-  )
-  parser.add_argument(
+    )
+    parser.add_argument(
       "--test_data",
       type=str,
       default="",
       help="Path to the test data."
-  )
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    )
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
