af561c2d4ee39a54b87c2b3ab37a38d8be9f9b0f
marshallford <inbox@marshallford.me>
2017-04-25 20:39:10 -0500

    s3 folder download/upload support

--- object_storage_tensorflow/example.py
+++ object_storage_tensorflow/example.py
@@ -1,7 +1,23 @@
 import object_storage_tensorflow as obj_tf
-# import os
+import os
 # from utils import ROOT_DIR
 
-testingFile = open('/tmp/testing.txt', 'r').read()
-obj_tf.s3.upload('my-test-bucket', 'foobar', testingFile)
-obj_tf.s3.download('my-test-bucket', 'foobar')
+
+# basic
+# testingFile = open('/tmp/testing.txt', 'r').read()
+# obj_tf.s3.upload('my-test-bucket', 'testing.txt', testingFile)
+# obj_tf.s3.download('my-test-bucket', 'testing.txt')
+
+# folders
+filenames = []
+data = []
+root = '/tmp/testing'
+folder = 'testing'
+for item in os.listdir(root):
+    path = os.path.join(root, item)
+    if os.path.isfile(path):
+        filenames.append(item)
+        data.append(open(path, 'r').read())
+
+obj_tf.s3.uploadFolder('my-test-bucket', folder, filenames, data)
+obj_tf.s3.downloadFolder('my-test-bucket', folder)
--- object_storage_tensorflow/s3.py
+++ object_storage_tensorflow/s3.py
@@ -1,4 +1,5 @@
 import boto3
+import os
 from . import config
 
 
@@ -14,8 +15,10 @@ def getConnection():
 
 def download(bucket, key):
     conn = getConnection()
-    # TODO: generate folder structure if required
-    path = '/tmp/object_storage_tensorflow/{}/{}'.format(bucket, key)
+    basePath = '/tmp/object_storage_tensorflow/{}'.format(bucket)
+    if not os.path.exists(basePath):
+        os.makedirs(basePath)
+    path = '{}/{}'.format(basePath, key)
     # TODO: return errors
     conn.Bucket(bucket).download_file(key, path)
     return path
@@ -26,3 +29,26 @@ def upload(bucket, key, data):
     # TODO: return errors
     conn.Bucket(bucket).put_object(Key=key, Body=data)
     return key
+
+
+def downloadFolder(bucket, folder):
+    conn = getConnection()
+    basePath = '/tmp/object_storage_tensorflow/{}/{}'.format(bucket, folder)
+    if not os.path.exists(basePath):
+        os.makedirs(basePath)
+    prefix = '{}/'.format(folder)
+    keys = []
+    for obj in conn.Bucket(bucket).objects.filter(Prefix=prefix):
+        path = '{}/{}'.format(basePath, obj.key.split('/', 1)[1])
+        conn.Bucket(bucket).download_file(obj.key, path)
+        keys.append(obj.key)
+    return keys
+
+
+def uploadFolder(bucket, prefix, keys, data):
+    fullkeys = []
+    for index, key in enumerate(keys):
+        fullKey = '{}/{}'.format(prefix, key)
+        upload(bucket, fullKey, data[index])
+        fullkeys.append(fullKey)
+    return fullkeys
